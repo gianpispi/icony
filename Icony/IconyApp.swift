c7585dba8ffa1d4ef2e0b06ea7faa310c76b8554
Gianpiero <gianpispi@me.com>
2021-11-28 23:24:56 +0100

    Initial Commit

---
+++ Icony/IconyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconyApp.swift
+//  Icony
+//
+//  Created by Gianpiero Spinelli on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct IconyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
